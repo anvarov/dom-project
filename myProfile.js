b90efaa25629e15eceb84bf865b9ab9efc88e8d3
Harrison Higgins <harryrhiggins@gmail.com>
2020-08-04 11:37:58 -0600

    Phase 2b

--- myProfile.js
+++ myProfile.js
@@ -5,7 +5,19 @@ window.addEventListener("DOMContentLoaded", () => {
     name.appendChild(nameText);
     document.body.appendChild(name);
 
-    const ulItems = ["DOB: 1990", "Last Name: Anvarov", "Project: dom-project", "Date: 08/04/2020"];
+    // const ulItems = ["DOB: 1990", "Last Name: Anvarov", "Project: dom-project", "Date: 08/04/2020"];
     const ul = document.createElement("ul");
 
+    // ulItems.forEach(item => {
+    //     const li = document.createElement('li');
+    //     const liText = document.createTextNode(item);
+    //     li.appendChild(liText);
+    //     ul.appendChild(li);
+    // })
+    document.body.appendChild(ul);
+
+    const ulItems = ["<li>DOB: 1990</li>", "<li>Last Name: Anvarov</li>", "<li>Project: dom-project</li>", "<li>Date: 08/04/2020</li>"];
+
+    const ulString = ulItems.join(' ');
+    ul.innerHTML = ulString;
 });
